61033cc1e65362471318cd8d3b6ad406547a2d27
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-21 22:53:33 -0400

    worked on the thoughts api

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought
+} = require('../../controllers/thoughtsController.js');
+
+
+router.route('/')
+.get(getThoughts)
+.post(createThought);
+
+router.route('/:thoughtId')
+.get(getSingleThought);
+
+
+
+
+module.exports = router;
