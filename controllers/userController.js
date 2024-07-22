6444508b3178fa3f6f2dc8dae4f4e9defa51018e
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-21 22:43:47 -0400

    userCOntroller mostly set up

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,37 @@ const getSingleUser = async (req, res) => {
       }
     };
 
+    const createUser = async (req, res) => {
+        try {
+            const user = await User.create(req.body);
+            res.status(201).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    };
+
+    const updateUser = async (req, res) => {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.userId, req.body, {new: true});
+            if (!user) return res.status(404).json({message: 'User not found'});
+            res.json(user);
+        } catch (err) {
+            res.stauts(500).json(err);
+        }
+    };
+
+    const deleteUser = async (req, res) => {
+        try {
+            const user = await User.findByIdAndDelete(req.params.userId);
+            if (!user) return res.status(404).json({message: 'User not found'});
+            res.json({ message: 'User and associated thoughts deleted' });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+    }
+
+    //will the thought persist?
+
 
 
     module.exports = {
