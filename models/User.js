3d30e596e8d4e592b372869481bc44621aa514b8
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-21 20:14:49 -0400

    worked on user.js model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a User model
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid email address'],
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('course', userSchema);
+
+module.exports = User;
